ac88b462b2afda592fb35a7bd8ff7d3a833f43ca
Shaheer Sajid <shaheersajid15@gmail.com>
2025-05-27 19:35:05 +0500

    added files for de10lite

---
+++ flows/synthesis/fpga/quartus/de10lite/bootmem/bootmem_inst.v
@@ -0,0 +1,6 @@
+bootmem	bootmem_inst (
+	.address ( address_sig ),
+	.clken ( clken_sig ),
+	.clock ( clock_sig ),
+	.q ( q_sig )
+	);
---
+++ flows/synthesis/fpga/quartus/de10lite/sim_uart.v
@@ -0,0 +1,17 @@
+module sim_uart
+(
+		input clk_i,
+		input write,
+		input [7:0]data
+);
+
+
+always @ (posedge clk_i) begin
+	if (write )
+	begin
+		$write("%c", data);
+		if(data == 35)
+		$stop;
+	end
+end
+endmodule
